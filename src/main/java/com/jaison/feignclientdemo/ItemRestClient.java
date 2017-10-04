098515f78d377d0533e903143bf11ff7eb16060f
JaisonJohn <Jaison.PoikayilJohn@target.com>
2017-10-04 03:41:04 -0500

    Adding the requestbody annotation

--- src/main/java/com/jaison/feignclientdemo/ItemRestClient.java
+++ src/main/java/com/jaison/feignclientdemo/ItemRestClient.java
@@ -3,11 +3,12 @@ package com.jaison.feignclientdemo;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(url="http://localhost:8080",name="ItemService")
 public interface ItemRestClient {
   @RequestMapping(value = "/items/{id}", method = RequestMethod.PUT)
-  String updateItem(@PathVariable("id") final Integer id, String request);
+  String updateItem(@PathVariable("id") final Integer id, @RequestBody String request);
 }
